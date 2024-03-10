98c3302240b5645be75f5e97e4af4a74a8ae3922
Dorin Munciuleanu <7981309-Muncidor@users.noreply.gitlab.com>
2024-03-10 23:51:48 +0100

    cargo doc

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -12,6 +12,17 @@ pub use server::Server;
 
 static INIT_TESTS: sync::Once = sync::Once::new();
 
+/// Initializes logging for tests.
+///
+/// This function initializes the logging system for running tests. It uses the `pretty_env_logger`
+/// crate to configure the logger with the following settings:
+///
+/// - `is_test(true)`: Indicates that the logger is being used in a test environment.
+/// - `parse_filters("info")`: Sets the log level to `info`, which means only log messages with a
+///   severity level of `info` or higher will be displayed.
+///
+/// This function is designed to be called once at the beginning of the test suite, ensuring that
+/// the logging system is properly configured before any tests are run.
 pub fn init_logging() {
     INIT_TESTS.call_once(|| {
         pretty_env_logger::formatted_builder()
